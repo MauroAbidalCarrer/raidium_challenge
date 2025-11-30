b42ddb3740e847ac51b82cf88952fdf38448841c
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-11-30 18:15:01 +0000

    Feat: Made models module ith mk_model function.

---
+++ src/models.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from monai.networks.nets import UNet
+
+from src.configs import TrainingConfig, ModelConfig
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def mk_model(train_cfg: TrainingConfig, model_cfg: ModelConfig) -> nn.Module:
+    model = (
+        UNet(
+            spatial_dims=2,
+            in_channels=1,
+            out_channels=train_cfg.n_classes,
+            channels=model_cfg.channels,
+            strides=model_cfg.strides,
+            kernel_size=model_cfg.kernel_size,
+            num_res_units=model_cfg.num_res_units,
+            act=model_cfg.act,
+            norm=model_cfg.norm,
+            dropout=model_cfg.dropout,
+            bias=model_cfg.bias,
+        )
+        .to(device)
+    )
+    model.cfg = model_cfg
+    return model

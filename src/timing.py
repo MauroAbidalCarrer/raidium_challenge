491476460ba242d56f465744b8eeebcc47af0a8d
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-12-03 16:28:46 +0000

    Clean: Removed total time print in timing.

--- src/timing.py
+++ src/timing.py
@@ -16,8 +16,5 @@ def time_to_run(context_name: str):
         time_dict[context_name] = time_dict.get(context_name, 0) + elapsed_time
 
 def print_time_dict():
-    time_sum = 0
     for k, v in time_dict.items():
         print(f"{k}: {v:.4f}s")
-        time_sum += v
-    print(f"\ntotals to: {time_sum:.2f}")

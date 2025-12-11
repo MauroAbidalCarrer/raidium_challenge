c29bc4a86cbf9a02faaeb6e589f6eec2bce7be34
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-12-11 17:33:31 +0000

    Feat-WIP: Ran the finetuning example notebook. Started writting the finetuning script.:

---
+++ run_sam_finetune.py
@@ -0,0 +1,68 @@
+import os
+import os
+import sys
+import math
+from tqdm import tqdm
+from pathlib import Path
+from typing import Any, overload
+from collections import defaultdict
+
+import torch
+import wandb
+import numpy as np
+import transformers 
+from scipy import ndimage
+from torch import nn, Tensor
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch.optim import Optimizer, AdamW
+from torch.utils.data import TensorDataset, DataLoader
+from torch.optim.lr_scheduler import LRScheduler, LambdaLR
+
+from src import (
+    dataset,
+    training,
+    metrics,
+    models,
+)
+from src import configs as cfg
+from src.plotting import plt_sample
+
+
+PRETRAINED_MODEL_ID = "facebook/sam-vit-base"
+def main():
+    dataset.mk_dataset(verbose=False)
+    train_cfg = cfg.TrainingConfig(
+        batch_size=16,
+        n_epochs=10,
+        test_size=0.1,
+        cross_entropy_loss_weight=1,
+        dice_loss_weight=2,
+        start_lr=1e-5,
+    )
+    processor = transformers.SamProcessor.from_pretrained(PRETRAINED_MODEL_ID)
+    model = transformers.SamModel.from_pretrained(PRETRAINED_MODEL_ID).to(cfg.DEVICE)
+    for name, param in model.named_parameters():
+        if name.startswith("vision_encoder") or name.startswith("prompt_encoder"):
+            param.requires_grad_(False)
+    model_cfg = 
+    optimizer = AdamW(
+        model.mask_decoder.parameters(),
+        lr=train_cfg.start_lr,
+        weight_decay=0,
+    )
+    lr_scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer, 1)
+    criterion = metrics.SegmentationLoss(train_cfg)
+    data_loaders = dataset.mk_data_loaders_for_finetuning()
+    trainer = training.Trainer(
+        model,
+        train_cfg,
+        optimizer,
+        lr_scheduler,
+        cfg.WandbConfig(["SAM", "finetuning"], "SAM")
+    )
+    trainer.train_model(data_loaders, criterion, "checkpoints/SAM/sam_epoch_{epoch}.pt")
+    
+    
+if __name__ == "__main__":
+    main()

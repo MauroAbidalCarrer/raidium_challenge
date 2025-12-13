dce2a1824f8b6dba348722a181bca427b15ab903
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-12-13 19:34:04 +0000

    Feat: Added the possibility to restart finething from a finetuning checkpoint instead of a pretraining one.

--- finetune_vit.py
+++ finetune_vit.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 import torch
@@ -13,13 +12,26 @@ def main():
     if len(sys.argv) < 2:
         print("ERROR: Please provide path to pretrained MAE ViT checkpoint.")
         exit(1)
-    chkpt = torch.load(sys.argv[1], weights_only=False)
-    optim_cfg = cfg.OPTIM_CFGS["finetuning"]
-    train_cfg = cfg.TRAIN_CONFIGS["finetuning"]
-    model_cfg = cfg.ModelConfig(**chkpt["model_cfg"])
-    model = models.mk_model_from_cfg(model_cfg)
-    model.load_state_dict(chkpt["model"])
-    optimizer = training.mk_optimizer(model, optim_cfg)
+    chkpt_pth = sys.argv[1]
+    chkpt = torch.load(chkpt_pth, weights_only=False)
+    if "pretraining" in chkpt_pth:
+        optim_cfg = cfg.OPTIM_CFGS["finetuning"]
+        train_cfg = cfg.TRAIN_CONFIGS["finetuning"]
+        model_cfg = cfg.ModelConfig(**chkpt["model_cfg"])
+        model = models.mk_model_from_cfg(model_cfg)
+        model.load_state_dict(chkpt["model"])
+        optimizer = training.mk_optimizer(model, optim_cfg)
+    elif "finetuning" in chkpt_pth:
+        optim_cfg = cfg.OPTIM_CFGS["finetuning"]
+        train_cfg = cfg.TrainingConfig(**chkpt["train_cfg"])
+        model_cfg = cfg.ModelConfig(**chkpt["model_cfg"])
+        model = models.mk_model_from_cfg(model_cfg)
+        model.load_state_dict(chkpt["model"])
+        optimizer = training.mk_optimizer(model, optim_cfg)
+        optimizer.load_state_dict(chkpt["optimizer"])
+    else:
+        print("Error: Don't know if this is a pretraining checkpoint or finetuning checkpoint please put it in appropriate folder")
+        exit(1)
 
     lr_scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer, 1)
     criterion = metrics.SegmentationLoss(train_cfg)
@@ -39,6 +51,10 @@ def main():
         lr_scheduler,
         wandb_run,
     )
+    if "finetuning" in chkpt_pth:
+        trainer.epoch = chkpt["epoch"]
+        trainer.ste = chkpt["ste"]
+        trainer.training_samples_seen = chkpt["training_samples_seen"]
     models.print_params_count(model)
     trainer.train_model(
         data_loaders,

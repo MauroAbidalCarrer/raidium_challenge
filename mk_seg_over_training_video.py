d40a915cbd32bd096eaa3c1e07333ed5e699c5b7
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-12-21 16:19:33 +0000

    Feat: Added argparse handling and saved both segementations pred AND barch in train_vid cache directory.

--- mk_seg_over_training_video.py
+++ mk_seg_over_training_video.py
@@ -1,15 +1,11 @@
 import os
-from sys import argv
-from tqdm import tqdm
-from IPython.display import HTML
+import argparse
 
 import torch
 import numpy as np
-import plotly.express as px
-from torch import nn, Tensor
-import matplotlib.colors as mcolors
+from torch import Tensor
+from rich.progress import track
 from matplotlib import pyplot as plt
-from torchvision.transforms import v2
 from matplotlib.colors import Normalize
 from matplotlib.animation import FuncAnimation
 
@@ -17,40 +13,62 @@ from src import configs as cfg
 from src import dataset, models
 
 
+MK_SEGS_TRACK_DESC = "Making segmentation preds over epochs"
 N_SAMPLES_TO_SHOW = 10
 N_CHKPT_TO_PLT = -1
-NO_ARGS_MSG = """
-Error: Please provide either path to:
-- a model checkpoints directory.
-- a cached segmentaiton .npy file.
-"""
 
 
 def main():
+    args = parse_args()
     dataset.mk_dataset()
+    if args.checkpoints is not None:
+        batch = mk_preprocessed_batch(50)
+        segs = mk_segs_preds_over_epochs(batch, args.checkpoints)
+        save_segs_and_batch(batch, segs)
+    elif args.vid_cache is not None:
+        print("Making animation from cached segmentation.")
+        segs = np.load(f"{args.vid_cache}/segs.npy")
+        batch = torch.load(f"{args.vid_cache}/batch.pt")
+    anim = mk_anim(batch, segs)
+    anim.save(
+        "segmentation_animation.mp4",
+        fps=30,
+        dpi=150,
+    )
+    print("Saved animation at ./segmentation_animation.mp4")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Segmentation visualization")
+    parser.add_argument(
+        "--checkpoints",
+        type=str,
+        help="Path to model checkpoint (.pt or .pth)",
+    )
+    parser.add_argument(
+        "--vid_cache",
+        type=str,
+        help="Path to cached video directory (contains segs.npy and batch.pt)",
+    )
+    args = parser.parse_args()
+    if args.checkpoints is None and args.vid_cache is None:
+        parser.error("You must specify either --checkpoints or --vid_cache")
+    if args.checkpoints is not None and args.vid_cache is not None:
+        parser.error("Please specify only one of --checkpoints or --vid_cache")
+    return args
+
+def mk_preprocessed_batch(batch_size: int) -> dict[str, Tensor]:
     train_cfg = cfg.TrainingConfig(batch_size=50)
     loaders = dataset.mk_segmentation_data_loaders(train_cfg)
     x, y_true = next(iter(loaders["train"]))
-    del loaders
     batch = dataset.preprocess_batch({"x": x, "y_true": y_true})
-    if len(argv) < 2:
-        print(NO_ARGS_MSG)
-        exit(1)
-    elif argv[1].endswith((".pt", ".pth")):
-        segs = mk_segs_over_training_array(batch, argv[1])
-    elif argv[1].endswith(".npy"):
-        print("Making animaiton from cached segmentation npy file.")
-        segs: np.ndarray = np.load(argv[1])
-    mk_anim_from_segs(batch, segs)
+    return batch
 
-def mk_segs_over_training_array(batch: dict[str, Tensor], chkpt_pth: str) -> np.ndarray:
-    segs = (
-        mk_segs_preds_over_epochs(batch, chkpt_pth)
-        .detach()
-        .cpu()
-        .numpy()
-    )
-    np.save("segs.npy", segs, allow_pickle=False)
+def save_segs_and_batch(batch: dict[str, Tensor], segs: np.ndarray) -> np.ndarray:
+    os.makedirs("video_cache", exist_ok=True)
+    batch = {k: v.detach().cpu() for k, v in batch.items()}
+    torch.save(batch, "video_cache/batch.pt")
+    np.save("video_cache/segs.npy", segs, allow_pickle=False)
+    print("Saved segmentations and batch in vid_cache directory.")
     return segs
 
 def mk_segs_preds_over_epochs(batch: dict[str, Tensor], chkpt_directory: str) -> Tensor:
@@ -61,7 +79,8 @@ def mk_segs_preds_over_epochs(batch: dict[str, Tensor], chkpt_directory: str) ->
         device=cfg.DEVICE,
     )
     with torch.no_grad():
-        for chkpt_idx, chkpt_filename in enumerate(tqdm(chkpt_filenames)):
+        ckpt_it = track(chkpt_filenames, MK_SEGS_TRACK_DESC)
+        for chkpt_idx, chkpt_filename in enumerate(ckpt_it):
             chkpt_pth = os.path.join(chkpt_directory, chkpt_filename)
             model = load_chkpt(chkpt_pth)
             with torch.autocast(cfg.DEVICE.type, torch.bfloat16):
@@ -70,7 +89,12 @@ def mk_segs_preds_over_epochs(batch: dict[str, Tensor], chkpt_directory: str) ->
                     .argmax(dim=1)
                     .to(dtype=torch.uint8)
                 )
-    return segs_buffer
+    return (
+        segs_buffer
+        .detach()
+        .cpu()
+        .numpy()
+    )
 
 def load_chkpt(chkpt_pth: str) -> torch.nn.Module:
     chkpt = torch.load(chkpt_pth, weights_only=False)
@@ -80,19 +104,12 @@ def load_chkpt(chkpt_pth: str) -> torch.nn.Module:
     model = model.eval()
     return model
 
-def mk_anim_from_segs(batch: dict[str, Tensor], segs: np.ndarray) -> FuncAnimation:
-    anim = mk_anim(batch, segs)
-    anim.save(
-        "segmentation_animation.mp4",
-        fps=30,
-        dpi=150,
-    )
-
 def mk_anim(
         batch: dict[str, Tensor],
         segs: np.ndarray,
         n_samples_to_plt: int = 6,
     ) -> FuncAnimation:
+    # Keep the first n_samples_to_plt
     x_np = (
         batch["x"]
         .cpu()
@@ -100,28 +117,17 @@ def mk_anim(
         [:n_samples_to_plt]
     )  # [B, H, W, C] or [B, H, W]
     segs = segs[:, :n_samples_to_plt]
-
-
     # For segmentation labels (categorical)
-    seg_norm = Normalize(
-        vmin=0,
-        vmax=cfg.N_CLASSES - 1,   # important: categorical, fixed range
-    )
+    seg_norm = Normalize(vmin=0, vmax=cfg.N_CLASSES - 1)
+    x_norm = Normalize(x_np.min(), x_np.max())
 
     colored_seg = plt.cm.rainbow(seg_norm(segs), bytes=True)  # -> [B, H, W, 4]
-
-    # For grayscale images
-    # Adjust depending on your preprocessing
-
-    x_norm = Normalize(
-        vmin=x_np.min(),
-        vmax=x_np.max(),
-    )
-    gray_x = plt.cm.gray(x_norm(x_np), bytes=True)  # -> [B, H, W, 4]
+    colored_x = plt.cm.gray(x_norm(x_np), bytes=True)  # -> [B, H, W, 4]
+    # Put the seg on top of the 
     seg_mask = (segs == 0)[..., None]  # background mask
     test_img_buffer = np.where(
         seg_mask,
-        gray_x[None, :, 0],
+        colored_x[None, :, 0],
         colored_seg,
     )
 
@@ -138,7 +144,7 @@ def mk_anim(
 
     seg_ims = []
     for j in range(batch_size):
-        axes[0, j].imshow(gray_x[j, 0])
+        axes[0, j].imshow(colored_x[j, 0])
         axes[0, j].set_title(f"Input {j}")
         axes[0, j].axis("off")
         im = axes[1, j].imshow(test_img_buffer[0, j])
